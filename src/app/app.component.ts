49243a3107a85210f9503b9e924608b3010deff0
braver90 <47883044+braver90@users.noreply.github.com>
2023-01-26 14:52:59 +0100

    3 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, VERSION } from '@angular/core';
+import { RxdbService } from './services/rxdb/rxdb.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  constructor(private rxbd: RxdbService) {}
   name = 'Angular ' + VERSION.major;
 }
---
+++ src/app/services/rxdb/rxdb.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { createRxDatabase, RxDatabase } from 'rxdb';
+import { getRxStorageDexie } from 'rxdb/plugins/dexie';
+import userSchema from './user.schema.json';
+@Injectable()
+export class RxdbService {
+  database: RxDatabase;
+  constructor() {
+    console.log(userSchema);
+  }
+
+  async initDatabase() {
+    this.database = await createRxDatabase({
+      name: 'testdb',
+      storage: getRxStorageDexie(),
+      password: 'myPassword',
+      multiInstance: true,
+      eventReduce: true,
+      cleanupPolicy: {},
+    });
+
+    this.database.addCollections({
+      users: {
+        schema: userSchema,
+      },
+    });
+  }
+}
